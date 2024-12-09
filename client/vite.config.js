e330c14e1a972b77ec6ddcf8a2750d4a5000f3d7
Aharon-zerbib <amzyt770@gmail.com>
2024-12-09 17:24:01 +0100

    test de proxi vite.json

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,10 +6,15 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
+      // En mode développement, redirige les appels vers le backend local
       "/api": {
-        target: "http://localhost:3001",
+        target:
+          process.env.NODE_ENV === "production"
+            ? "https://ton-backend-en-production.com" // URL de ton API en production
+            : "http://localhost:3001", // URL du backend local en développement
         changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""), // Optionnel : cela supprime '/api' dans le chemin de la requête
       },
     },
   },
-});
+  
